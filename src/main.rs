5f181f38a0a2eb147e49c66b3e721353b35c3e1d
Sungbae Jeong <almagest0328@gmail.com>
2023-08-24 13:24:26 +0900

    fix html bug

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,5 @@ fn main() {
     dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
 
     log::info!("starting app");
-    //log::info!("song datas: {:?}", SONG_DATAS.get());
     dioxus_web::launch(app::App);
 }
